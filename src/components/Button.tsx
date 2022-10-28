dd79b40e731a733baa9a549d6e5fbb0cd02bb886
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2022-10-28 21:19:43 +0330

    fix button hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,10 @@ const Button = (props) => {
   return (
     <MUIButton
       {...props}
-      className={clsx(props.className, "bg-blue-700 text-white")}
+      className={clsx(
+        props.className,
+        "bg-blue-700 text-white hover:bg-blue-800"
+      )}
     />
   );
 };
